1c5c3a6c51574c4cdc3721a60cf29ca63ff6029c
krishd9895 <105974502+krishd9895@users.noreply.github.com>
2025-05-28 18:59:36 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -8,5 +8,5 @@ def home():
     return "I'm alive"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Get PORT from environment, fallback to 5000
+    port = int(os.environ.get('PORT', 5002))  # Get PORT from environment, fallback to 5000
     app.run(host='0.0.0.0', port=port)
